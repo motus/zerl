c868b75aa11eee7dbbe97af7c644c421248a4de1
Sergei Matusevich <sergei.matusevich@gmail.com>
2010-09-21 22:38:16 -0700

    added tests/zerl_tests.erl; other minor fixes; compiles, but not tested yet

--- test/zerl_tests.erl
+++ test/zerl_tests.erl
@@ -1,31 +1,23 @@
 
 -module(zerl_tests).
 
-start(Params) ->
-  [Dir, Port] = Params,
-  Options = [{base_dir, Dir}, {port, list_to_integer(Port)}],
-  error_logger:info_msg("- zml_srv:start/1: ~p~n", [Options]),
-  error_logger:info_msg("- zml_srv:start/1: PID: ~p~n", [Pid]),
-  error_logger:info_report([{Name, {Path, Ts, IsStatic}}
-    || {Name, Path, Ts, _Templ, IsStatic} <- ets:tab2list(zml_templates)]).
-
-% stop misultin
-stop() -> misultin:stop().
+-export([handle_http/1]).
 
 % callback function called on incoming http request
-handle_http(Req, Options) ->
-  handle(Req:get(method), Req:resource([lowercase, urldecode]), Req, Options).
+handle_http(Req) ->
+  error_logger:info_report(Req),
+  handle(Req:get(method), Req:resource([lowercase, urldecode]), Req).
 
 % handle a GET on /Page
-handle('GET', [], Req, _Options) ->
+handle('GET', [], Req) ->
   Req:respond(302, [{"Location", "/index.zml"}], []);
 
-handle('GET', _, Req, Options) ->
+handle('GET', _, Req) ->
   {abs_path, Path} = Req:get(uri),
   error_logger:info_msg("= GET: ~s~n", [Path]),
   case string:right(Path, 5) of
-    [_|".zml"] -> Req:ok(zml:render(Path, Options));
-    _ -> BaseDir = proplists:get_value(base_dir, Options, "."),
+    [_|".zml"] -> Req:ok(zml:render(Path));
+    _ -> BaseDir = application:get_env(base_dir),
          Req:file(BaseDir ++ "/" ++ Path)
   end.
 
