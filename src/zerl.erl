212a492d2f5ed7ef805e068406cae974ed70daf8
Sergei Matusevich <sergei.matusevich@gmail.com>
2010-09-21 17:31:00 -0700

    first version of the zerl application - compiles ok but not tested yet

--- src/zerl.erl
+++ src/zerl.erl
@@ -1,42 +1,33 @@
 
 -module(zerl).
 
--export([start_link/0, start/1, stop/0]).
+-export([start_link/0, stop/0]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 start_link() ->
-  %% Start misultin, zml, and epgsql_pool here?
-  {ok, self()}.
-
-% start misultin http server
-start(Params) ->
-  [Dir, Port] = Params,
-  Options = [{base_dir, Dir}, {port, list_to_integer(Port)}],
-  error_logger:info_msg("- zml_srv:start/1: ~p~n", [Options]),
   zml:start(),
-  zml:template_dir("", Options),
-  {ok, Pid} = misultin:start_link(
-    [{loop, fun(Req) -> handle_http(Req, Options) end} | Options]),
-  error_logger:info_msg("- zml_srv:start/1: PID: ~p~n", [Pid]),
-  error_logger:info_report([{Name, {Path, Ts, IsStatic}}
-    || {Name, Path, Ts, _Templ, IsStatic} <- ets:tab2list(zml_templates)]).
+  lists:foreach(fun zml:template_dir/1,
+    default(application:get_env(zml_templates), ["."])),
+  misultin:start_link(
+    get_fun(handle_http_module, handle_http,    loop) ++
+    get_fun(handle_ws_module,   handle_ws,   ws_loop) ++
+    application:get_all_env(misultin)).
 
-% stop misultin
 stop() -> misultin:stop().
 
-% callback function called on incoming http request
-handle_http(Req, Options) ->
-  handle(Req:get(method), Req:resource([lowercase, urldecode]), Req, Options).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% handle a GET on /Page
-handle('GET', [], Req, _Options) ->
-  Req:respond(302, [{"Location", "/index.zml"}], []);
+default(undefined, Def) -> Def;
+default(Val,      _Def) -> Val.
 
-handle('GET', _, Req, Options) ->
-  {abs_path, Path} = Req:get(uri),
-  error_logger:info_msg("= GET: ~s~n", [Path]),
-  case string:right(Path, 5) of
-    [_|".zml"] -> Req:ok(zml:render(Path, Options));
-    _ -> BaseDir = proplists:get_value(base_dir, Options, "."),
-         Req:file(BaseDir ++ "/" ++ Path)
-  end.
+get_fun(Mod, Fun, Prop) ->
+  get_fun_impl(application:get_env(Mod), Fun, Prop).
+
+get_fun_impl(undefined, _Fun, _Prop) -> [];
+
+get_fun_impl(ModName, Fun, Prop) ->
+  {module, Mod} = code:ensure_loaded(application:get_env(ModName)),
+  true = erlang:function_exported(Mod, Fun, 1),
+  [{Prop, fun(Req) -> apply(Mod, Fun, [Req]) end}].
 
