2dc24925e670d1af42b470803625a2abfdc6a75c
Sergei Matusevich <sergei.matusevich@gmail.com>
2010-09-24 01:02:29 -0700

    removed some old debug code from zerl startup

--- src/zerl.erl
+++ src/zerl.erl
@@ -7,18 +7,13 @@
 
 start_link() ->
   lists:foreach(fun application:start/1,
-    [crypto, ssl, epgsql, epgsql_pool, zml]),
+    [crypto, ssl, epgsql, epgsql_pool, zml, misultin]),
   lists:foreach(fun zml:template_dir/1,
     default(application:get_env(zml_templates), [""])),
-  error_logger:info_report([{Name, {Path, Ts, IsStatic}}
-    || {Name, Path, Ts, IsStatic} <- ets:tab2list(zml_templates)]),
-  application:start(misultin),
-  Options =
+  misultin:start_link(
     get_fun(handle_http_module, handle_http,    loop) ++
     get_fun(handle_ws_module,   handle_ws,   ws_loop) ++
-    application:get_all_env(misultin),
-  error_logger:info_report(Options),
-  misultin:start_link(Options).
+    application:get_all_env(misultin)).
 
 start() -> start_link().
 
